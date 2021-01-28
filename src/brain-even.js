f18a78834ebd3ae3882c4e22bd82a0c804377aa0
Rinat Khabibullin <r.s.khabibullin@mail.ru>
2021-01-28 22:44:06 +0300

    rename function getQuestionAndAnswer

--- src/brain-even.js
+++ src/brain-even.js
@@ -7,12 +7,12 @@ const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min)
 
 const task = 'What is the result of the expression?';
 
-const evenGame = () => {
+const getQuestionAndAnswer = () => {
   const question = getRandomInt(1, 100);
   const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
-const playGame = () => engine(task, evenGame);
+const playGame = () => engine(task, getQuestionAndAnswer);
 
 export default playGame;
