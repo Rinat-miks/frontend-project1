de05d3ab2d71e91fb688f78a74b8a67124e10fed
Rinat Khabibullin <r.s.khabibullin@mail.ru>
2021-01-28 23:44:46 +0300

    add prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// eslint-disable-next-line import/extensions
+import startPlay from '../src/brain-prime.js';
+
+startPlay();
---
+++ src/brain-prime.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/extensions */
+import engine from './index.js';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  if (number % 2 === 0) {
+    return false;
+  }
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const number = getRandomInt(1, 100);
+  const answer = isPrime(number) ? 'yes' : 'no';
+  const question = `${number}`;
+  return [question, answer];
+};
+
+const playGame = () => engine(task, getQuestionAndAnswer);
+
+export default playGame;
