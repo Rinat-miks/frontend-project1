220ac6ab189ad3371c06ba5baa7c678cccfa1053
Rinat Khabibullin <r.s.khabibullin@mail.ru>
2021-01-28 23:43:49 +0300

    add progression game

---
+++ src/brain-progression.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/extensions */
+import engine from './index.js';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const task = 'What number is missing in the progression?';
+
+const getProgression = () => {
+  const countOfProgression = 10;
+  const firstNumberOfProgression = getRandomInt(1, 10);
+  const progression = [firstNumberOfProgression];
+  const progressionOperand = getRandomInt(1, 10);
+  for (let i = 0; i < countOfProgression - 1; i += 1) {
+    progression.push(progression[i] + progressionOperand);
+  }
+  return progression;
+};
+
+const getQuestionAndAnswer = () => {
+  const missingNumberIndex = getRandomInt(0, 9);
+  const progression = getProgression();
+  const answer = progression[missingNumberIndex];
+  const question = `${progression.slice(0, missingNumberIndex).join(' ')} .. ${progression.slice(missingNumberIndex + 1).join(' ')}`;
+  return [question, answer];
+};
+
+const playGame = () => engine(task, getQuestionAndAnswer);
+
+export default playGame;
